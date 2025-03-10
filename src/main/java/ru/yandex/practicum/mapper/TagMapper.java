d7c33d66d7cd9cac08c21b18c025b655ec00c08e
Grigory <grigoriy.trubnikov@ebs.ru>
2025-03-10 14:35:59 +0300

    feat: обновление маппера

--- src/main/java/ru/yandex/practicum/mapper/TagMapper.java
+++ src/main/java/ru/yandex/practicum/mapper/TagMapper.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.mapper;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import ru.yandex.practicum.dao.Tag;
 import ru.yandex.practicum.dto.TagDto;
@@ -13,9 +11,4 @@ import ru.yandex.practicum.dto.TagDto;
 @Mapper(componentModel = "spring")
 public interface TagMapper {
   List<TagDto> toDto(Iterable<Tag> tags);
-  default Set<Long> mapTagToTagIds(Set<Tag> tags) {
-    return tags.stream()
-               .map(Tag::getId)
-               .collect(Collectors.toSet());
-  }
 }
